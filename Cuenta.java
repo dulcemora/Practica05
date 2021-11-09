632b7f5969c94515a74915d48c8315e585bd16c1
dulcemora <dulcemora03@ciencias.unam.mx>
2021-11-09 11:22:08 -0600

    Modifique los param de el primer constructor

--- Cuenta.java
+++ Cuenta.java
@@ -1,7 +1,7 @@
-/*Clase que nos da el titular y dinero disponible de una cuenta
+/**Clase que nos da el titular y dinero disponible de una cuenta
 * @author Dulce Mora
 * @version 1.0
-*/
+**/
 public class Cuenta {
    
     //Atributos
@@ -10,51 +10,44 @@ public class Cuenta {
     //El dinero disponible de la cuenta
     public double dineroDisponible = 0;
 
-    /*Constructor con valores definidos
-     * @param titular El parametro que representa el nombre del titular, que por ser predeterminado es Dulce Mora
-     * @param dineroDisponible El parametro que representa el dinero disponible de la cuenta que en este caso es 1070.20
-     */
+    /**Constructor con valores definidos titular (Dulce Mora) y dinero disponible(0) 
+    **/
     public Cuenta() {
     this.titular = "Dulce Mora";
     this.dineroDisponible = 1070.20;
     }
     
-   /*Constructor que recibe un valor String
+    /**Constructor que recibe un valor String y da dineroDisponible en 0
      * @param titular El parametro que representa el nombre del titular
-     * @param dineroDisponible El parametro que representa el dinero disponible y en este caso es 0
-     */
+     **/
     public Cuenta(String titular) {
     this.titular = titular;
     this.dineroDisponible = 0;
     }
 
-    /*Metodo que devuelve el titular
+    /**Metodo que devuelve el titular
      *@return El nombre del titular
-     */
+     **/
     public String getTitular(){
     return this.titular;
     }
-    /*Metodo que modifica el nombre del titular
-     */
+    //Metodo que modifica el nombre del titular 
     public void setTitular(String titular){
     this.titular = titular;
     }
     
-    /*Metodo que devuelve el dinero disponible
+    /**Metodo que devuelve el dinero disponible
      *@return El dinero disponible de la cuenta
-     */
+     **/
     public double getDineroDisponible(){
     return this.dineroDisponible;
     }
-    /*Metodo que modifica el dinero disponible
-     */
+    //Metodo que modifica el dinero disponible
     public void setDineroDisponible(double dineroDisponible){
     this.dineroDisponible = dineroDisponible;
     }
 
-    /*toString de la clase
-    *Nos devuelve el nombre del titular y el dinero disponible
-    */
+    //toString de la clase: Nos devuelve el nombre del titular y el dinero disponible
     public String toString() {
     return "Nombre del titular: " + titular + "\n" + "Dinero Disponible: " + dineroDisponible;
    }
